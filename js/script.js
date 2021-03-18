1a07c2bb39b117d1f8b1068feb1fef33f1b34e10
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-18 15:02:52 +0100

    uso clone per il div template

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //Todolist
+//Creo un Array di cose da fare
+
+var toDoList = [
+  'Comprare il vino',
+  'Portare fuori il cane',
+  'Chiamare Francesco'
+];
+
+var template = $('.template li').clone();
+console.log(template);
